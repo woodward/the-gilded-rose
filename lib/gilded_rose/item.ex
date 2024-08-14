63fd8b81a6942bf6326e0a8553cc70d33f8fba8c
Greg Woodward <woodward@users.noreply.github.com>
2024-08-14 14:16:27 -0700

    Add type info to Item

--- lib/gilded_rose/item.ex
+++ lib/gilded_rose/item.ex
@@ -1,5 +1,16 @@
 defmodule GildedRose.Item do
-  defstruct name: nil, sell_in: nil, quality: nil
+  @moduledoc false
+  @type t :: %__MODULE__{
+          name: String.t(),
+          sell_in: integer(),
+          quality: non_neg_integer()
+        }
+
+  defstruct [
+    :name,
+    :sell_in,
+    :quality
+  ]
 
   def new(name, sell_in, quality) do
     %__MODULE__{name: name, sell_in: sell_in, quality: quality}
