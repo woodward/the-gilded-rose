3a2676c09b9f729c6328b33eeb8abfeb0fd52e9a
Greg Woodward <woodward@users.noreply.github.com>
2024-08-15 12:43:11 -0700

    Add the cases for negative sell_in

--- lib/gilded_rose/item/generic.ex
+++ lib/gilded_rose/item/generic.ex
@@ -3,10 +3,15 @@ defmodule GildedRose.Item.Generic do
 
   @behaviour GildedRose.Inventory
 
+  alias GildedRose.Item
   alias GildedRose.Quality
 
   @impl true
-  def increment_age_by_1_day(item) do
+  def increment_age_by_1_day(%Item{sell_in: sell_in} = item) when sell_in <= 0 do
+    %{item | sell_in: item.sell_in - 1, quality: Quality.decrease_by_two(item.quality)}
+  end
+
+  def increment_age_by_1_day(%Item{} = item) do
     %{item | sell_in: item.sell_in - 1, quality: Quality.decrease_by_one(item.quality)}
   end
 end
--- test/gilded_rose/item/generic_test.exs
+++ test/gilded_rose/item/generic_test.exs
@@ -13,5 +13,29 @@ defmodule GildedRose.Item.GenericTest do
 
       assert_item(item, sell_in: 9, quality: 19)
     end
+
+    test "decreases the quality by two once the sell in goes negative" do
+      item = %Item{name: "+5 Dexterity Vest", sell_in: 0, quality: 20}
+
+      item = Generic.increment_age_by_1_day(item)
+
+      assert_item(item, sell_in: -1, quality: 18)
+    end
+
+    test "decreases the quality until zero" do
+      item = %Item{name: "+5 Dexterity Vest", sell_in: 10, quality: 1}
+
+      item = Generic.increment_age_by_1_day(item)
+
+      assert_item(item, sell_in: 9, quality: 0)
+    end
+
+    test "decreases the quality until zero - negative sell-in" do
+      item = %Item{name: "+5 Dexterity Vest", sell_in: 0, quality: 1}
+
+      item = Generic.increment_age_by_1_day(item)
+
+      assert_item(item, sell_in: -1, quality: 0)
+    end
   end
 end
